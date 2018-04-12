9e4fe548a6721508a275e329126428279e3bc1de
jeffrey k eliasen <jeff@jke.net>
2018-04-12 13:45:37 -1000

    framework for unittests

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from setuptools import find_packages, setup
 
 
@@ -13,7 +15,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 
 install_dependencies = (
-    'Django>=1.8',
+    'Django>=1.8' + (',<1.11.99' if sys.version_info[0] < 3 else ''),
 )
 
 
@@ -48,4 +50,5 @@ setup(
     ],
     install_requires=install_dependencies,
     tests_require=install_dependencies + ('mock',),
+    test_suite='runtests.run_tests',
 )
---
+++ tests/settings.py
@@ -0,0 +1,29 @@
+import django
+
+DEBUG = True
+USE_TZ = True
+
+SECRET_KEY = 'abcdefghijklmnopqrstuvwxyz'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'tests.urls'
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'django.contrib.redirects',
+]
+
+SITE_ID = 1
+
+if django.VERSION >= (1, 10):
+    MIDDLEWARE = ()
+else:
+    MIDDLEWARE_CLASSES = ()
