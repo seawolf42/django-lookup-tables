46651835cad6a1b6eae5f05820688f777bc401d9
jeffrey k eliasen <jeff@jke.net>
2018-06-05 10:34:04 -1000

    refactor: rename property

--- lookup_tables/fields.py
+++ lookup_tables/fields.py
@@ -5,8 +5,8 @@ from . import models
 
 class _lookup_table_item_choice_iterator(object):
 
-    def __init__(self, table):
-        self.items = models.LookupTableItem.objects.filter(table=table).iterator()
+    def __init__(self, lookuptable):
+        self.items = models.LookupTableItem.objects.filter(table=lookuptable).iterator()
 
     def __iter__(self):
         return self
@@ -21,17 +21,17 @@ class _lookup_table_item_choice_iterator(object):
 
 class _lookup_table_item_choices(object):
 
-    def __init__(self, table):
-        self.table = table
+    def __init__(self, lookuptable):
+        self.lookuptable = lookuptable
 
     def __iter__(self):
-        return _lookup_table_item_choice_iterator(self.table)
+        return _lookup_table_item_choice_iterator(self.lookuptable)
 
 
 class LookupTableItemField(db_models.Field):
 
     def __init__(self, table_ref, *args, **kwargs):
-        self.table = models.LookupTable.objects.filter(table_ref=table_ref).first()
-        if not self.table:
-            self.table = models.LookupTable.objects.create(table_ref=table_ref, name=table_ref)
+        self.lookuptable = models.LookupTable.objects.filter(table_ref=table_ref).first()
+        if not self.lookuptable:
+            self.lookuptable = models.LookupTable.objects.create(table_ref=table_ref, name=table_ref)
         super(LookupTableItemField, self).__init__(*args, **kwargs)
