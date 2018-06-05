b89ba3c91b72eb1e1758b0b66d9aedff67f3941f
jeffrey k eliasen <jeff@jke.net>
2018-06-05 12:56:32 -1000

    inherit from ForeignKey and adjust tests accordingly

--- lookup_tables/fields.py
+++ lookup_tables/fields.py
@@ -3,35 +3,23 @@ from django.db import models as db_models
 from . import models
 
 
-class _lookup_table_item_choice_iterator(object):
-
-    def __init__(self, lookuptable):
-        self.items = models.LookupTableItem.objects.filter(table=lookuptable).iterator()
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        item = next(self.items)
-        return (item.id, item.name)
-
-    def next(self):
-        return self.__next__()
-
-
-class _lookup_table_item_choices(object):
-
-    def __init__(self, lookuptable):
-        self.lookuptable = lookuptable
-
-    def __iter__(self):
-        return _lookup_table_item_choice_iterator(self.lookuptable)
-
-
-class LookupTableItemField(db_models.Field):
+class LookupTableItemField(db_models.ForeignKey):
 
     def __init__(self, table_ref, *args, **kwargs):
+        self.table_ref = table_ref
         self.lookuptable = models.LookupTable.objects.filter(table_ref=table_ref).first()
         if not self.lookuptable:
             self.lookuptable = models.LookupTable.objects.create(table_ref=table_ref, name=table_ref)
+        kwargs['to'] = 'lookup_tables.LookupTableItem'
+        kwargs['on_delete'] = db_models.PROTECT
+        kwargs['limit_choices_to'] = self.get_lookuptableitem_choices
         super(LookupTableItemField, self).__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(LookupTableItemField, self).deconstruct()
+        del kwargs['limit_choices_to']
+        kwargs['table_ref'] = self.table_ref
+        return name, path, args, kwargs
+
+    def get_lookuptableitem_choices(self):
+        return db_models.Q(table=self.lookuptable)
--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.db import models as db_models
 from django.test import TestCase
 
@@ -6,57 +8,48 @@ from lookup_tables import models
 
 from . import utils
 
+if sys.version_info[0] < 3:
+    # python 2
+    import mock
+else:
+    # python 3
+    from unittest import mock
+
 
 strings = utils.strings
 
 
-class TestLookupTableItemChoicesIterators(TestCase):
+class LookupTableItemField(TestCase):
 
-    def setUp(self):
-        self.table = models.LookupTable.objects.create(table_ref=strings[0], name=strings[0])
-        self.values = [
-            models.LookupTableItem.objects.create(table=self.table, name=strings[i], sort_order=i)
-            for i in range(len(strings) - 1)
-        ]
+    @mock.patch('django.db.models.ForeignKey.__init__')
+    def test_construction(self, mock_fk_init):
+        self.assertEqual(models.LookupTable.objects.filter(name=strings[0]).count(), 0)
+        item = fields.LookupTableItemField(table_ref=strings[0])
+        self.assertEqual(models.LookupTable.objects.filter(name=strings[0]).count(), 1)
+        self.assertEqual(item.table_ref, strings[0])
+        self.assertEqual(item.lookuptable, models.LookupTable.objects.get())
 
-    def test_iterator(self):
-        iterator = fields._lookup_table_item_choice_iterator(self.table)
-        for i, choice in enumerate(iterator):
-            self.assertEqual(choice[0], self.values[i].pk)
-            self.assertEqual(choice[1], self.values[i].name)
-
-    def test_iterator_only_loads_items_from_selected_table(self):
-        table2 = models.LookupTable.objects.create(table_ref=strings[-1], name=strings[-1])
-        models.LookupTableItem.objects.create(table=table2, name=strings[0], sort_order=0)
-        iterator = fields._lookup_table_item_choice_iterator(self.table)
-        for i, choice in enumerate(iterator):
-            self.assertEqual(choice[0], self.values[i].pk)
-            self.assertEqual(choice[1], self.values[i].name)
-
-    def test_wrapper(self):
-        iterator = fields._lookup_table_item_choices(self.table)
-        for i, choice in enumerate(iterator):
-            self.assertEqual(choice[0], self.values[i].pk)
-            self.assertEqual(choice[1], self.values[i].name)
-
-    def test_wrapper_lazy_loads(self):
-        iterator = fields._lookup_table_item_choices(self.table)
-        self.values.append(
-            models.LookupTableItem.objects.create(table=self.table, name=strings[-1], sort_order=len(strings))
+    @mock.patch('django.db.models.ForeignKey.__init__')
+    def test_kwargs_includes_foreign_key_requisite_values(self, mock_fk_init):
+        item = fields.LookupTableItemField(table_ref=strings[0])
+        mock_fk_init.assert_called_once_with(
+            to='lookup_tables.LookupTableItem',
+            on_delete=db_models.PROTECT,
+            limit_choices_to=item.get_lookuptableitem_choices,
         )
-        for i, choice in enumerate(iterator):
-            self.assertEqual(choice[0], self.values[i].pk)
-            self.assertEqual(choice[1], self.values[i].name)
 
-    def test_iterator_honors_sort_order(self):
-        for i, value in enumerate(self.values):
-            value.sort_order = len(strings) - i
-            value.save()
-        self.values.reverse()
-        iterator = fields._lookup_table_item_choices(self.table)
-        for i, choice in enumerate(iterator):
-            self.assertEqual(choice[0], self.values[i].pk)
-            self.assertEqual(choice[1], self.values[i].name)
+    @mock.patch('django.db.models.ForeignKey.__init__')
+    def test_get_lookuptable_choices_returns_q_object(self, mock_fk_init):
+        item = fields.LookupTableItemField(table_ref=strings[0])
+        self.assertIsInstance(item.get_lookuptableitem_choices(), db_models.Q)
+
+    def test_deconstruct_removes_programmatic_args(self):
+        item = fields.LookupTableItemField(table_ref=strings[0])
+        name, path, args, kwargs = item.deconstruct()
+        self.assertTrue('to' in kwargs)
+        self.assertTrue('on_delete' in kwargs)
+        self.assertFalse('limit_choices_to' in kwargs)
+        self.assertEqual(kwargs['table_ref'], strings[0])
 
 
 class TestLookupTableItemFieldCreation(TestCase):
