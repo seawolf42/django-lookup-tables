97a00336ddfab60ac64a7c849c324b6c236e20e5
jeffrey k eliasen <jeff@jke.net>
2018-06-14 10:21:57 -1000

    updated setting for DRF usage

--- manage.py
+++ manage.py
@@ -1,7 +1,10 @@
 import os
 import sys
 
+from django.core.management import execute_from_command_line
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
-    from django.core.management import execute_from_command_line
+    os.environ.setdefault('LOOKUP_TABLES_DRF_FIELD_INIT_NO_RESET', str(sys.argv[1] != 'runserver'))
     execute_from_command_line(sys.argv)
