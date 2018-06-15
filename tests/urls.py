d75ea94b7b6a805ca4f9f8d86e26c0ba38860e3d
jeffrey k eliasen <jeff@jke.net>
2018-06-15 09:19:28 -1000

    include urls for sample app

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,19 @@
+from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 from django.http import HttpResponse
 
+from rest_framework.routers import DefaultRouter
+
+from sample_app import views
+
+
+router = DefaultRouter()
+router.register(r'mymodels', views.MyModelViewSet, base_name='mymodel')
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include((router.urls, 'sample_app'), namespace='api')),
     url(r'^$', lambda req: HttpResponse('OK')),
 ]
