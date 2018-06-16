3c1b424eaa9b252ed85417ea222a526edc5ccc34
jeffrey k eliasen <jeff@jke.net>
2018-06-16 07:46:57 -1000

    versioned

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_dependencies = (
 
 setup(
     name='django-lookup-tables',
-    version='0.13.0',
+    version='0.13.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
