c0a2e6ea0f1fd2f97f963b82c0af8d80842fb35a
jeffrey k eliasen <jeff@jke.net>
2018-08-09 17:02:06 -1000

    remove all deprecated code

--- lookup_tables/fields.py
+++ lookup_tables/fields.py
@@ -1,10 +1,5 @@
-import warnings
-
 from django.apps import apps
 from django.db import models as db_models
-
-from . import conf
-from . import models
 
 
 class LookupField(db_models.ForeignKey):
@@ -31,52 +26,13 @@ class LookupField(db_models.ForeignKey):
         return db_models.Q()
 
     def get_default_lookup(self):
-        if _IGNORE_FIELD_DEFAULT_LOOKUPS or not apps.ready:
+        if not apps.ready:
             return None
         return None
         # return self.table.objects.filter(is_default=True).first()
 
 
-_IGNORE_FIELD_DEFAULT_LOOKUPS = conf.IS_RUNNING_MANAGEMENT
-
-
 class LookupTableItemField(db_models.ForeignKey):
 
-    def __init__(self, table_ref, *args, **kwargs):
-        warnings.warn('LookupTableItemField is being removed in version 0.15.0; read Betas.md for more info')
-        self.table_ref = table_ref
-        self._is_initialized = False
-        kwargs['to'] = 'lookup_tables.LookupTableItem'
-        kwargs['on_delete'] = db_models.PROTECT
-        kwargs['limit_choices_to'] = self.get_lookuptableitem_choices
-        kwargs['default'] = self.get_default_lookuptableitem
-        kwargs['related_name'] = '+'
-        super(LookupTableItemField, self).__init__(*args, **kwargs)
-
-    def deconstruct(self):
-        name, path, args, kwargs = super(LookupTableItemField, self).deconstruct()
-        if 'limit_choices_to' in kwargs:
-            del kwargs['limit_choices_to']
-        if 'default' in kwargs:
-            del kwargs['default']
-        kwargs['table_ref'] = self.table_ref
-        return name, path, args, kwargs
-
-    def get_lookuptableitem_choices(self):
-        if not self._is_initialized:
-            self._init_lookuptable()
-        return db_models.Q(table__table_ref=self.table_ref)
-
-    def get_default_lookuptableitem(self):
-        if _IGNORE_FIELD_DEFAULT_LOOKUPS or not apps.ready:
-            return None
-        if not self._is_initialized:
-            self._init_lookuptable()
-        return models.LookupTableItem.objects.filter(table__table_ref=self.table_ref, is_default=True).first()
-
-    def _init_lookuptable(self):
-        lookuptable = models.LookupTable.objects.filter(table_ref=self.table_ref).first()
-        if not lookuptable:
-            lookuptable = models.LookupTable.objects.create(table_ref=self.table_ref, name=self.table_ref)
-            models.LookupTableItem.objects.create(table=lookuptable, name='<DEFAULT>')
-        self._is_initialized = True
+    def __init__(self, *args, **kwargs):
+        raise Exception('this class is no longer supported; see Betas.md for upgrade information')
--- lookup_tables/models.py
+++ lookup_tables/models.py
@@ -1,9 +1,5 @@
-import warnings
-
-from django.core.exceptions import ValidationError
 from django.db import models
 from django.db import transaction
-from django.utils.text import slugify
 
 
 class AbstractLookupTable(models.Model):
@@ -28,88 +24,10 @@ class AbstractLookupTable(models.Model):
 class LookupTable(models.Model):
 
     def __init__(self, *args, **kwargs):
-        warnings.warn('LookupTable is being removed in version 0.15.0; read Betas.md for more info')
-        super(LookupTable, self).__init__(*args, **kwargs)
-
-    table_ref = models.CharField(max_length=100, unique=True, editable=False)
-    name = models.CharField(max_length=100, unique=True)
-
-    default = models.ForeignKey('LookupTableItem', null=True, blank=True, on_delete=models.PROTECT)
-
-    class Meta:
-        verbose_name = 'Lookup Table'
-        ordering = ('name',)
-
-    def _clean_table_ref(self):
-        if self.table_ref:
-            return
-        self.table_ref = slugify(self.name)
-
-    def _clean_default(self):
-        if self.default and self.default.table != self:
-            raise ValidationError('default must be child of this table')
-
-    def clean(self):
-        self._clean_table_ref()
-        self._clean_default()
-
-    def save(self, *args, **kwargs):
-        with transaction.atomic():
-            self.full_clean()
-            previous_default = LookupTable.objects.get(pk=self.pk).default if self.pk else None
-            changed_previous = previous_default != self.default
-            if previous_default and changed_previous:
-                previous_default.is_default = False
-                previous_default.save()
-            super(LookupTable, self).save(*args, **kwargs)
-            if changed_previous and self.default:
-                self.default.is_default = True
-                self.default.save()
-
-    def __str__(self):
-        return self.name
+        raise Exception('this class is no longer supported; see Betas.md for upgrade information')
 
 
 class LookupTableItem(models.Model):
 
     def __init__(self, *args, **kwargs):
-        warnings.warn('LookupTableItem is being removed in version 0.15.0; read Betas.md for more info')
-        super(LookupTableItem, self).__init__(*args, **kwargs)
-
-    table = models.ForeignKey(LookupTable, on_delete=models.PROTECT, editable=False)
-    name = models.CharField(max_length=100)
-
-    sort_order = models.PositiveSmallIntegerField(default=0)
-
-    is_default = models.BooleanField(default=False, blank=False, editable=False)
-
-    class Meta:
-        verbose_name = 'Lookup Table Item'
-        unique_together = ('table', 'name')
-        ordering = ('sort_order',)
-
-    def _clean_table(self):
-        if not self.id:
-            return
-        previous = LookupTableItem.objects.get(pk=self.pk)
-        if previous.table != self.table:
-            raise ValidationError('cannot change table name on existing lookup values')
-
-    def _clean_is_default(self):
-        if self.is_default:
-            # if self.table.default != self:
-            #     raise ValidationError('is_default must match table default item')
-            if LookupTableItem.objects.filter(table=self.table, is_default=True).exclude(id=self.id).count() > 0:
-                raise ValidationError('cannot set is_default on multiple items for the same table')
-
-    def clean(self):
-        self._clean_table()
-        self._clean_is_default()
-
-    def save(self, *args, **kwargs):
-        with transaction.atomic():
-            self.full_clean()
-            super(LookupTableItem, self).save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
+        raise Exception('this class is no longer supported; see Betas.md for upgrade information')
